7d5292e5b19499ae8e099fd9fd83102194bca10e
Jordan Hall <jhall@Jordans-MacBook-Air.local>
2025-11-13 16:34:27 -0500

    ember corrections

--- threads-api/U0000013063_fib.c
+++ threads-api/U0000013063_fib.c
@@ -3,37 +3,18 @@
 #include <pthread.h>
 #include "common_threads.h"
 
-typedef struct {
-    int a;
-    int b;
-} myarg_t;
-
-typedef struct {
-    int x;
-    int y;
-} myret_t;
-
-    int fibb(n){
-        int f[n]; f[0]=0;
-        if ( n > 0 ){
-        f[1] = 1;
-        for (int i = 2; i<=n; i++) {
-        f[i] = f[i-1] + f[i-2];
-        }
-        return f[n];
-        }
-    }
-
 void *mythread(void *arg) {
     int *args = (int *) arg;
-    printf("args %d\n", args);
     int *fib = malloc(sizeof(int) * (*args + 1));
     assert(fib != NULL);
     fib[0]=0;
-    if ( *args > 0 ){
+    if ( *args > 0 )
+    {
         fib[1] = 1;
-        for (int i = 2; i<=*args; i++) {
-        fib[i] = fib[i-1] + fib[i-2];
+        int i = 2;
+        for (i = 2; i<=*args; i++)
+        {
+            fib[i] = fib[i-1] + fib[i-2];
         };
     }
     return (void *) fib;
@@ -49,8 +30,7 @@ int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, mythread, &n);
     Pthread_join(p, (void **) &fib);
-    printf("Fibbonacci N: %d\n", fib[n]);
+    printf("Fibbonacci N: %d\n", fib[n-1]);
     free(fib);
     return 0;
 }
-
